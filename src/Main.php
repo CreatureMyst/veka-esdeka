7eddbe300eb09a06530c66aeedc46245ec367024
Dima Trish <dtrish@space-o.ru>
2016-08-24 18:49:30 +0700

    Guzzle client

--- src/Main.php
+++ src/Main.php
@@ -2,14 +2,28 @@
 
 namespace Creaturemyst\VekaEsdeka;
 
+use GuzzleHttp\Client;
+
 class Main
 {
+    const
+        ENDPOINT_AUTH = 'https://oauth.vk.com/',
+        ENDPOINT_API = 'https://api.vk.com/';
+
     private $clientId;
     private $clientSecret;
     private $accessToken;
 
+    /** @var Client */
+    protected $apiClient;
+
+    /** @var Client */
+    protected $authClient;
+
     public function __construct()
     {
+        $this->apiClient = new Client(['base_uri' => self::ENDPOINT_API]);
+        $this->authClient = new Client(['base_uri' => self::ENDPOINT_AUTH]);
     }
 
     /**
